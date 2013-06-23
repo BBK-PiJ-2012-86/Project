85fa2808f2571ad54d76cb20cb7d37316ad79bd6
BBK-PiJ-2012-86 <ruthroygbiv@yahoo-co-uk>
2013-06-23 12:36:30 +0100

    done makeCross

--- src/new_approach/WH.java
+++ src/new_approach/WH.java
@@ -8,4 +8,18 @@ public class WH {
 		position.and(input);
 		return position.cardinality()%2==1;
 	}
+	
+	public static BitSet makeCross(BitSet input, int length) {
+		BitSet result = new BitSet(length*length);
+		for (int i = 0; i< length; i++) {
+			if(input.get(i)) {
+				for (int j = 0; j<length; j++) {
+					if (input.get(j)) {
+						result.set(i*length+j);
+					}
+				}
+			}
+		}
+		return result;
+	}
 }
