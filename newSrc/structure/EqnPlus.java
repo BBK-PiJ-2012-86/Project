424123b253fc2f8e65cea021b3b800fa653674f7
BBK-PiJ-2012-86 <ruthroygbiv@yahoo-co-uk>
2013-06-24 21:14:07 +0100

    Made EqnPlus extend Eqn

--- newSrc/structure/EqnPlus.java
+++ newSrc/structure/EqnPlus.java
@@ -3,15 +3,10 @@ package structure;
 import java.util.BitSet;
 import java.util.Random;
 
-public class EqnPlus {
-	private int numVars;
-	private BitSet coeffs;	// canonical ordering, cancelled and xij is st i<=j
-	private boolean rhs;
+public class EqnPlus extends Eqn {
 		
 	public EqnPlus(int numVars, BitSet coeffs, boolean rhs) {
-		this.numVars = numVars;
-		this.coeffs = coeffs;
-		this.rhs = rhs;
+		super(numVars, coeffs, rhs);
 		niceify();
 	}
 	/*public EqnPlus(int numVars, BitSet coeffs, boolean rhs, boolean niceify) {	//just for testing
@@ -20,75 +15,15 @@ public class EqnPlus {
 		this.rhs = rhs;
 		if (niceify) niceify();
 	}*/
-	public int getNumVars() {
-		return numVars;
-	}
-	public BitSet getCoeffs() {
-		return coeffs;
-	}
+	@Override
 	public void setCoeffs(BitSet coeffs) {
-		this.coeffs = coeffs;
+		super.setCoeffs(coeffs);
 		niceify();
 	}
-	public boolean getRhs() {
-		return rhs;
-	}
-	public void setRhs(boolean rhs) {
-		this.rhs = rhs;
-	}
-	
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((coeffs == null) ? 0 : coeffs.hashCode());
-		result = prime * result + numVars;
-		result = prime * result + (rhs ? 1231 : 1237);
-		return result;
-	}
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (!(obj instanceof EqnPlus))
-			return false;
-		EqnPlus other = (EqnPlus) obj;
-		if (coeffs == null) {
-			if (other.coeffs != null)
-				return false;
-		} else if (!coeffs.equals(other.coeffs))
-			return false;
-		if (numVars != other.numVars)
-			return false;
-		if (rhs != other.rhs)
-			return false;
-		return true;
-	}
-	@Override
-	public String toString() {
-		StringBuilder builder = new StringBuilder();
-		boolean started = false;
-		if (coeffs!=null) {
-			for (int n = 0; n< coeffs.length(); n++) {
-				if(coeffs.get(n)) {
-					if(started) {builder.append(" + ");	}
-					builder.append("x");
-					builder.append(n/numVars+1);
-					builder.append("x");
-					builder.append(n%numVars+1);
-					started = true;
-				}
-			}
-		}
-		builder.append(" = ");
-		builder.append(rhs?"1":"0");
-		return builder.toString();
-	}
-	
-		
+
 	private void niceify() {
+		int numVars = getNumVars();
+		BitSet coeffs = getCoeffs();
 		if (coeffs == null) {return;}
 		BitSet newCoeffs = new BitSet(numVars*numVars);
 		int n, m, diag;
@@ -101,7 +36,7 @@ public class EqnPlus {
 				newCoeffs.set(n,coeffs.get(n)^coeffs.get(m));
 			}
 		}
-		coeffs = newCoeffs;
+		super.setCoeffs(newCoeffs);
 	}
 	
 	public static EqnPlus make(int numVars) {
@@ -139,11 +74,5 @@ public class EqnPlus {
 		coeffs.clear(crossSize,crossSize+64);
 		return new EqnPlus(numVars, coeffs, rand.nextBoolean(), false);
 	}*/
-	
-	@Override
-	public Object clone() {
-		return new EqnPlus(numVars, (BitSet)coeffs.clone(), rhs);
-	}
-	
 
 }
